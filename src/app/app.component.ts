cbff603813c41df2b2c548f0a87951a736735150
Natasha Draper <natasha@draper.net.za>
2021-06-22 19:48:04 +0200

    func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'jc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'jcalc';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  public natContr?: number;
+  public krisContr?: number;
+  public natLeft?: number;
+  public krisLeft?: number;
+  public totalContr?: number;
+
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+  private loadNum(key: string): number | null {
+    const val = localStorage.getItem(key);
+    return val ? parseFloat(val) : null;
+  }
+
+  private loadValues() {
+    const needs = this.loadNum('needs');
+    const natasha = this.loadNum('natasha');
+    const kristen = this.loadNum('kristen');
+    this.form.patchValue({ needs, kristen, natasha });
+  }
+
+  private persistValues(needs: number, kristen: number, natasha: number) {
+    if (needs) {
+      localStorage.setItem('needs', needs.toString());
+    }
+    if (kristen) {
+      localStorage.setItem('kristen', kristen.toString());
+    }
+    if (natasha) {
+      localStorage.setItem('natasha', natasha.toString());
+    }
+  }
+
+  ngOnInit() {
+    const val = Validators.compose([Validators.required, Validators.min(0)]);
+    this.form = this.formBuilder.group({
+      needs: [null, val],
+      kristen: [null, val],
+      natasha: [null, val],
+    });
+    this.form.valueChanges.subscribe(({ needs, kristen, natasha }) => this.persistValues(needs, kristen, natasha));
+    this.form.valueChanges
+      .pipe(filter(({ needs, kristen, natasha }) => !!needs && !!kristen && !!natasha))
+      .subscribe(() => this.calculate());
+    this.loadValues();
+  }
+
+  public calculate() {
+    const { needs, kristen, natasha } = this.form.value;
+    this.natContr = (natasha - kristen + needs) / 2;
+    this.natLeft = natasha - this.natContr;
+    this.krisContr = needs - this.natContr;
+    this.krisLeft = kristen - this.krisContr;
+    this.totalContr = this.krisContr + this.natContr;
+  }
+
+  public reset() {
+    localStorage.clear();
+    this.form.reset();
+    this.natContr = undefined;
+    this.natLeft = undefined;
+    this.krisContr = undefined;
+    this.krisLeft = undefined;
+    this.totalContr = undefined;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
